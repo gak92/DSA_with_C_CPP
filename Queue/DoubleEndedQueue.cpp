3ef1d7e0f1f99a1770857594e626c4192d19be12
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-07 21:59:15 +0500

    Initialize basic Structure of Double Ended Queue

---
+++ Queue/DoubleEndedQueue.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int isEmpty()
+{
+}
+
+int isFull()
+{
+}
+
+void enqueueFront()
+{
+}
+
+void enqueueRear()
+{
+}
+
+int dequeueFront()
+{
+}
+
+int dequeueRear()
+{
+}
+
+int main()
+{
+
+  return 0;
+}
