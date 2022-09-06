2de7ec3c903252b4ae9d0da798fbeb4055c512de
gak92 <44802922+gak92@users.noreply.github.com>
2022-09-06 23:10:45 +0500

    Linked representation of a binary tree

---
+++ Trees/BinaryTree.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct node
+{
+  int data;
+  struct node *left;
+  struct node *right;
+};
+
+struct node *createNode(int data)
+{
+  struct node *n = (struct node *)malloc(sizeof(struct node));
+  n->data = data;
+  n->left = NULL;
+  n->right = NULL;
+  return n;
+}
+
+int main()
+{
+  struct node *p = createNode(2);
+  struct node *p1 = createNode(1);
+  struct node *p2 = createNode(4);
+
+  p->left = p1;
+  p->right = p2;
+
+  return 0;
+}
