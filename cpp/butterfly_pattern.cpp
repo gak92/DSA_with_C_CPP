4b5a7f371083f024fbd6ddef9b34549da5541f3f
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-01 20:05:59 +0500

    Solved butterfly pattern

---
+++ cpp/butterfly_pattern.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num = 5;
+  int spaces;
+
+  for (int i = 1; i <= num; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+    spaces = 2 * num - 2 * i;
+
+    for (int j = 1; j <= spaces; j++)
+    {
+      cout << " ";
+    }
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+
+  for (int i = num; i >= 1; i--)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+    spaces = 2 * num - 2 * i;
+
+    for (int j = 1; j <= spaces; j++)
+    {
+      cout << " ";
+    }
+    for (int j = 1; j <= i; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
