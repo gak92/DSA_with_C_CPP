2b8029c2dcd96dc1277b84260d25c3b550aa22ac
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-01 19:34:15 +0500

    Add half pyramid 180 degree and print pyramid of numbers

--- cpp/pattern2.cpp
+++ cpp/pattern2.cpp
@@ -17,5 +17,32 @@ int main()
     cout << "\n";
   }
 
+  // Half Pyramid after 180 degree rotation
+  for (int i = 1; i <= num; i++)
+  {
+    for (int j = 1; j <= num; j++)
+    {
+      if (j <= num - i)
+      {
+        cout << " ";
+      }
+      else
+      {
+        cout << "*";
+      }
+    }
+    cout << endl;
+  }
+
+  // Half pyramid using numbers
+  for (int i = 1; i <= num; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << i;
+    }
+    cout << endl;
+  }
+
   return 0;
 }
