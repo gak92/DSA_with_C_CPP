aa98a2aa1bb805b8c57a9bdd9dd3a3ad8e187e3b
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-02 12:01:43 +0500

    Solved rhombus pattern

---
+++ cpp/Pattern Questions/rhombus_pattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num = 5;
+
+  for (int i = 1; i <= num; i++)
+  {
+    for (int k = 1; k <= num - i; k++)
+    {
+      cout << " ";
+    }
+    for (int j = 1; j <= num; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+  return 0;
+}
