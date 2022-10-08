51acbcf851cf0f091dba6b721e8563f27b9d2558
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-09 01:32:10 +0500

    Implement nesting of functions

--- cpp/OOP/01_check_binary.cpp
+++ cpp/OOP/01_check_binary.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 class binary
 {
+  // private:
   string s;
+  void check_binary(void);
 
 public:
   void read_binary(void);
-  void check_binary(void);
+  void ones_complement(void);
+  void display(void);
 };
 
 void binary ::read_binary(void)
@@ -29,12 +32,38 @@ void binary ::check_binary(void)
   cout << "a binary number";
 }
 
+void binary ::ones_complement(void)
+{
+  check_binary();
+  for (int i = 0; i < s.length(); i++)
+  {
+    if (s.at(i) == '0')
+      s.at(i) = '1';
+    else
+      s.at(i) = '0';
+  }
+}
+
+void binary ::display(void)
+{
+  for (int i = 0; i < s.length(); i++)
+  {
+    cout << s.at(i);
+  }
+  cout << "" << endl;
+}
+
 int main()
 {
 
   binary b;
   b.read_binary();
-  b.check_binary();
+
+  cout << "\nBinary number: ";
+  b.display();
+  b.ones_complement();
+  cout << "\nOnes complement: ";
+  b.display();
 
   return 0;
 }
