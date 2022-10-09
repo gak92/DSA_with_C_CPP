3b73536665f5f784772f82ee7a76d93ec651ec72
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-10 02:17:44 +0500

    Creating array of objects

--- cpp/OOP/04_arrays_of_objects.cpp
+++ cpp/OOP/04_arrays_of_objects.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 using namespace std;
 
+class Employee
+{
+  int id;
+  int salary;
+
+public:
+  void setId(int empid)
+  {
+    id = empid;
+    salary = 111;
+  }
+
+  void getId(void)
+  {
+    cout << "The id of employee: " << id << endl;
+  }
+};
+
 int main()
 {
+  Employee fb[4];
+
+  for (int i = 0; i < 4; i++)
+  {
+    fb[i].setId(i);
+    fb[i].getId();
+  }
 
   return 0;
 }
