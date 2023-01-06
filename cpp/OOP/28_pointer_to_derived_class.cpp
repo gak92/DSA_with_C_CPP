c3271e3bff895c2103e6191aea2845395bc6b37c
Ghazanfar Ali <gak_92@hotmail.com>
2023-01-06 18:49:57 +0500

    base class pointer point to derived class object

---
+++ cpp/OOP/28_pointer_to_derived_class.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+  public:
+    int var_base;
+    void display(void)
+    {
+      cout << "Value of var_base: " << var_base << endl;
+    }
+};
+
+class Derived : public Base
+{
+  public:
+  int var_derived;
+  void display(void)
+  {
+    cout << "Value of var_base: " << var_base << endl;
+    cout << "Value of var_derived: " << var_derived << endl;
+  }
+};
+
+int main()
+{
+  Base base_obj;
+  Derived derived_obj;
+
+  Base *base_ptr;
+  base_ptr = &derived_obj;
+
+  base_ptr->var_base = 34;
+  base_ptr->display();
+
+  Derived *derived_ptr;
+  derived_ptr = &derived_obj;
+  derived_ptr->var_base = 19;
+  derived_ptr->var_derived = 41;
+  derived_ptr->display();
+
+
+  return 0;
+}
