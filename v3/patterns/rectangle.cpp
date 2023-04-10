422bf5d3b980b53141e8e333487771c667f85f8c
Ghazanfar Ali <gak_92@hotmail.com>
2023-04-10 19:56:53 +0500

    Pattern - Rectangle

---
+++ v3/patterns/rectangle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i, j, n;
+  cout << "Enter n: ";
+  cin >> n;
+
+  i = 1;
+  while (i <= n)
+  {
+    j = 1;
+    while (j <= n)
+    {
+      cout << "*";
+      j = j + 1;
+    }
+
+    cout << endl;
+    i = i + 1;
+  }
+
+  return 0;
+}
