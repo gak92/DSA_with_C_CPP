e5cf59188bfc8def48fb28d5e838b0f4334dc850
Ghazanfar Ali <gak_92@hotmail.com>
2023-04-13 18:45:30 +0500

    Pattern - print star

---
+++ v3/patterns/star3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+/*
+Enter n: 5
+*****
+ ****
+  ***
+   **
+    *
+*/
+
+int main()
+{
+  int row, col, n, space;
+  cout << "Enter n: ";
+  cin >> n;
+
+  row = 1;
+  while (row <= n)
+  {
+    space = row - 1;
+    while (space)
+    {
+      cout << " ";
+      space = space - 1;
+    }
+
+    col = 1;
+    while (col <= n - row + 1)
+    {
+      cout << "*";
+      col = col + 1;
+    }
+    cout << endl;
+    row = row + 1;
+  }
+
+  return 0;
+}
