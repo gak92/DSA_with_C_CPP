32366285b9d5c036f7968e661b9917ae7adb541e
Ghazanfar Ali <gak_92@hotmail.com>
2023-04-21 23:58:39 +0500

    Find intersection of two arrays using optimized approach

--- v3/Arrays/intersection_two_array.cpp
+++ v3/Arrays/intersection_two_array.cpp
@@ -3,12 +3,18 @@
 #include <vector>
 using namespace std;
 
-int main()
+void print_array(vector<int> arr)
 {
-  vector<int> arr1 = {1, 2, 2, 2, 3, 4};
-  vector<int> arr2 = {2, 2, 3, 3};
-  vector<int> res;
+  for (int i = 0; i < arr.size(); i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
 
+vector<int> find_intersection1(vector<int> arr1, vector<int> arr2)
+{
+  vector<int> res;
   for (int i = 0; i < arr1.size(); i++)
   {
     int element = arr1[i];
@@ -23,12 +29,48 @@ int main()
     }
   }
 
-  cout << "Intersection of two Array: ";
-  for (int i = 0; i < res.size(); i++)
+  return res;
+}
+
+vector<int> find_intersection2(vector<int> arr1, vector<int> arr2)
+{
+  vector<int> res;
+  int i = 0, j = 0;
+
+  while (i < arr1.size() && j < arr2.size())
   {
-    cout << res[i] << " ";
+    if (arr1[i] == arr2[j])
+    {
+      res.push_back(arr1[i]);
+      i++;
+      j++;
+    }
+    else if (arr1[i] < arr2[j])
+    {
+      i++;
+    }
+    else
+    {
+      j++;
+    }
   }
-  cout << endl;
+
+  return res;
+}
+
+int main()
+{
+  vector<int> arr1 = {1, 2, 2, 2, 3, 4};
+  vector<int> arr2 = {2, 2, 3, 3};
+  vector<int> res;
+
+  cout << "Intersection of two Array (Brute Force): ";
+  res = find_intersection1(arr1, arr2);
+  print_array(res);
+
+  cout << "Intersection of two Array (Optimized): ";
+  res = find_intersection2(arr1, arr2);
+  print_array(res);
 
   return 0;
 }
