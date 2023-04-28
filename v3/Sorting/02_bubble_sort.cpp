6d36061d1b6f11c9ddd53462461a49b2c500a1cb
Ghazanfar Ali <gak_92@hotmail.com>
2023-04-28 20:08:53 +0500

    Optimized bubble sort

--- v3/Sorting/02_bubble_sort.cpp
+++ v3/Sorting/02_bubble_sort.cpp
@@ -16,12 +16,18 @@ void bubbleSort(vector<int> arr)
 
   for (int i = 0; i < n - 1; i++)
   {
+    bool swapped = false;
     for (int j = 0; j < n - i - 1; j++)
     {
       if (arr[j] > arr[j + 1])
       {
         swap(arr[j], arr[j + 1]);
+        swapped = true;
       }
+    }
+    if (!swapped)
+    {
+      break;
     }
   }
 
