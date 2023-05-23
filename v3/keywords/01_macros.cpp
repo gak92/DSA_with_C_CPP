95c75f05c0e65d1f0b035e8a0124b7f0435803ba
Ghazanfar Ali <gak_92@hotmail.com>
2023-05-23 14:08:11 +0500

    Using macros

---
+++ v3/keywords/01_macros.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#define PI 3.142
+
+int main()
+{
+  int r = 5;
+  int area = PI * r * r;
+  cout << "Area of a cricle: " << area << endl;
+
+  return 0;
+}
