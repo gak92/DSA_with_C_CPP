a56f45134433751c3924634f9c571e82ac4a672a
Ghazanfar Ali <gak_92@hotmail.com>
2023-05-26 19:10:19 +0500

    use getter and setter for private properties

--- v3/OOPs/01_intro.cpp
+++ v3/OOPs/01_intro.cpp
@@ -3,23 +3,35 @@ using namespace std;
 
 class Hero
 {
-public:
+private:
   int health;
+
+public:
   char level;
 
   void print()
   {
     cout << "level: " << level << endl;
   }
+
+  void setHealth(int h)
+  {
+    health = h;
+  }
+
+  int getHealth()
+  {
+    return health;
+  }
 };
 
 int main()
 {
   Hero john;
-  john.health = 90;
+  john.setHealth(90);
   john.level = 'A';
 
-  cout << "Johne health: " << john.health << endl;
+  cout << "Johne health: " << john.getHealth() << endl;
   cout << "Johne level: " << john.level << endl;
 
   return 0;
