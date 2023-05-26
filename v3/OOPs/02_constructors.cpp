70c4e0d13d17e86b1940ee01b7e344193e7c5f0d
Ghazanfar Ali <gak_92@hotmail.com>
2023-05-26 20:15:09 +0500

    default, parametrized and copy constructor

---
+++ v3/OOPs/02_constructors.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+/*
+  1- Default Constructor
+  2- Parametrized Constructor
+  3- Copy Constructor
+*/
+
+class Hero
+{
+private:
+  int health;
+
+public:
+  char level;
+
+  // default constructor
+  Hero()
+  {
+    cout << "constructor called" << endl;
+  }
+
+  // parametrized constructor
+  Hero(int health)
+  {
+    cout << "Address of this: " << this << endl;
+    this->health = health;
+  }
+
+  Hero(int health, char level)
+  {
+    this->health = health;
+    this->level = level;
+  }
+
+  // copy constructor
+  Hero(Hero &h)
+  {
+    cout << "Copy constructor called... " << endl;
+    this->health = h.health;
+    this->level = h.level;
+  }
+
+  void print()
+  {
+    cout << "level: " << this->level << endl;
+    cout << "health: " << this->health << endl;
+  }
+
+  void setHealth(int h)
+  {
+    health = h;
+  }
+
+  int getHealth()
+  {
+    return health;
+  }
+};
+
+int main()
+{
+  // static allocation
+  cout << "Befor constructor called" << endl;
+  Hero john(70);
+  cout << "Address of john: " << &john << endl;
+  cout << "After constructor called" << endl;
+
+  cout << endl;
+
+  // dynamically
+  cout << "Befor constructor called" << endl;
+  Hero *mike = new Hero(90);
+  cout << "Address of mike: " << mike << endl;
+  cout << "After constructor called" << endl;
+
+  cout << endl;
+
+  // calling copy constructor
+  Hero h1(40, 'C');
+  h1.print();
+
+  Hero h2(h1);
+  h2.print();
+
+  return 0;
+}
