fa3d109ee5d8308bebe73bfd2c5c34a55728ad23
Ghazanfar Ali <gak_92@hotmail.com>
2023-05-27 17:08:30 +0500

    Singly linked list - insertion at head

---
+++ v3/LinkedList/01_singly_linked_list.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+
+  // constructur
+  Node(int data)
+  {
+    this->data = data;
+    this->next = NULL;
+  }
+};
+
+void printLL(Node *&head)
+{
+  Node *temp = head;
+
+  while (temp != NULL)
+  {
+    cout << temp->data << " ";
+    temp = temp->next;
+  }
+
+  cout << endl;
+}
+
+void insertAtHead(Node *&head, int data)
+{
+  Node *temp = new Node(data);
+  temp->next = head;
+  head = temp;
+}
+
+int main()
+{
+  Node *node1 = new Node(5);
+
+  cout << "Node address: " << node1 << endl;
+  cout << "Node data: " << node1->data << endl;
+  cout << "Next node address: " << node1->next << endl;
+
+  Node *head = node1;
+
+  cout << "Linked list is: ";
+  printLL(head);
+
+  insertAtHead(head, 12);
+  cout << "Linked list is: ";
+  printLL(head);
+
+  insertAtHead(head, 7);
+  cout << "Linked list is: ";
+  printLL(head);
+
+  return 0;
+}
