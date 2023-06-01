4b71a822946d1d438234057329ffdeafbf474627
Ghazanfar Ali <gak_92@hotmail.com>
2023-06-01 16:22:57 +0500

    delete middle element from the stack

---
+++ v3/stack/05_delete_mid_element_from_stack.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+void solve(stack<int> &inputStack, int count, int size)
+{
+  // base case
+  if (count == size / 2)
+  {
+    inputStack.pop();
+    return;
+  }
+
+  int num = inputStack.top();
+  inputStack.pop();
+
+  // recursive calls
+  solve(inputStack, count + 1, size);
+
+  inputStack.push(num);
+}
+
+void deleteMiddle(stack<int> &inputStack, int N)
+{
+
+  // Write your code here
+  int count = 0;
+  solve(inputStack, count, N);
+}
+
+int main()
+{
+
+  return 0;
+}
