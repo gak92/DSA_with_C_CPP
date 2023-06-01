3e5ef1c3ae33fc3d6b5ba9840f7ff47c131324ef
Ghazanfar Ali <gak_92@hotmail.com>
2023-06-01 16:48:33 +0500

    valid parenthesis using stack

---
+++ v3/stack/06_valid_parenthesis.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+bool isValidParenthesis(string expression)
+{
+  // Write your code here.
+  stack<char> st;
+
+  for (int i = 0; i < expression.length(); i++)
+  {
+    char ch = expression[i];
+    if (ch == '(' || ch == '{' || ch == '[')
+    {
+      st.push(ch);
+    }
+    else
+    {
+      if (!st.empty())
+      {
+        char top = st.top();
+        if ((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '['))
+        {
+          st.pop();
+        }
+        else
+        {
+          return false;
+        }
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+
+  if (st.empty())
+    return true;
+  else
+    return false;
+}
+
+int main()
+{
+
+  return 0;
+}
