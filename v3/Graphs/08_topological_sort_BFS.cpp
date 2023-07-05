550cb7892e40dab3ddd64a561d36dc4bee014342
Ghazanfar Ali <gak_92@hotmail.com>
2023-07-05 17:14:27 +0500

    toplogical sort using BFS

---
+++ v3/Graphs/08_topological_sort_BFS.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)
+{
+  // Write your code here
+  // create adjacency list
+  unordered_map<int, list<int>> adj;
+  for (int i = 0; i < e; i++)
+  {
+    int u = edges[i][0];
+    int v = edges[i][1];
+
+    adj[u].push_back(v);
+  }
+
+  // find all indegrees
+  vector<int> indegrees(v);
+  for (auto i : adj)
+  {
+    for (auto j : i.second)
+    {
+      indegrees[j]++;
+    }
+  }
+
+  // push 0 indegree
+  queue<int> q;
+  for (int i = 0; i < v; i++)
+  {
+    if (indegrees[i] == 0)
+      q.push(i);
+  }
+
+  // Do BFS
+  vector<int> res;
+  while (!q.empty())
+  {
+    int frontNode = q.front();
+    q.pop();
+
+    res.push_back(frontNode);
+
+    // neighbor indegree update
+    for (auto nebor : adj[frontNode])
+    {
+      indegrees[nebor]--;
+      if (indegrees[nebor] == 0)
+      {
+        q.push(nebor);
+      }
+    }
+  }
+
+  return res;
+}
