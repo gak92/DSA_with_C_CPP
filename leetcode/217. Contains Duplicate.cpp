64bf4980b0fd9b8bd32ac281e6d10a5c0e8563e8
Ghazanfar Ali <gak_92@hotmail.com>
2024-03-17 00:29:26 +0500

    217 contains duplicate

---
+++ leetcode/217. Contains Duplicate.cpp
@@ -0,0 +1,56 @@
+/*
+https://leetcode.com/problems/contains-duplicate/description/
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+*/
+
+#include <stdio.h>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  bool containsDuplicate(vector<int> &nums)
+  {
+    unordered_map<int, int> uniq;
+
+    for (auto num : nums)
+    {
+      if (uniq[num] >= 1)
+        return true;
+      uniq[num] += 1;
+    }
+    return false;
+  }
+};
+
+int main()
+{
+  Solution s;
+  vector<int> nums = {1, 2, 3, 1};
+  printf("%d\n", s.containsDuplicate(nums));
+  return 0;
+}
