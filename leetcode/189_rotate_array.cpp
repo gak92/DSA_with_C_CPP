1c6e4fa601254107803a71930c61d18bd5fac1e3
Ghazanfar Ali <gak_92@hotmail.com>
2024-03-18 18:36:51 +0500

    189 Rotate Array

--- leetcode/189_rotate_array.cpp
+++ leetcode/189_rotate_array.cpp
@@ -1,5 +1,6 @@
 /*
 189. Rotate Array
+https://leetcode.com/problems/rotate-array/description/
 
 Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 
@@ -20,6 +21,9 @@ Explanation:
 rotate 1 steps to the right: [99,-1,-100,3]
 rotate 2 steps to the right: [3,99,-1,-100]
 */
+// ==========================
+// Solution using extra space
+// ==========================
 
 #include <iostream>
 #include <bits/stdc++.h>
@@ -54,5 +58,50 @@ int main()
   cout << "Rotated Array: ";
   print_vector(temp);
 
+  return 0;
+}
+
+// ==================================
+// Solution using reverse
+// ==================================
+
+class Solution
+{
+public:
+  void reverse(vector<int> &nums, int left, int right)
+  {
+    int temp;
+    while (left <= right)
+    {
+      temp = nums[left];
+      nums[left] = nums[right];
+      nums[right] = temp;
+      left++;
+      right--;
+    }
+  }
+
+  void rotate(vector<int> &nums, int k)
+  {
+    int n = nums.size();
+    int left = 0;
+    int right = n - 1;
+    k = k % n;
+
+    reverse(nums, left, right); // reverse whole array
+    reverse(nums, 0, k - 1);    // reverse from start to k - 1
+    reverse(nums, k, n - 1);    // reverse remaining
+
+    // for(int i=0; i<n; i++)
+    //     cout << nums[i] << ", ";
+  }
+};
+
+int main()
+{
+  Solution s;
+  vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+  int k = 3;
+  s.rotate(nums, k);
   return 0;
 }
